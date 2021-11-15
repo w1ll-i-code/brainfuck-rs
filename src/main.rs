e119b74f95aba607e1851795ea7d8ba24341ed57
William Calliari <william.calliari@student.uibk.ac.at>
2021-11-15 13:43:55 +0100

    Added config options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod ast;
 
 fn main() {
+    let config: Config = match structopt::StructOpt::from_args_safe() {
+        Ok(val) => val,
+        Err(err) => {
+            eprintln!("Could not parse commandline arguments:  {}", err.message);
+            return;
+        }
+    };
 
-    let mut file = std::fs::read_to_string("./test.bf").unwrap();
-    file.retain(|c| ['+', '-', '<', '>', '[', ']', '.', ','].contains(&c));
+    let a = ast::parse(&config).ok().unwrap();
 
     let a = ast::parse(&file).ok().unwrap();
     ast::parse(a.0).unwrap_err();
